ec7688a8b9d10e2308d94cce34b1b0ecd2284fb8
Anıl TOPUZ <aniltopuz@gmail.com>
2022-09-29 15:21:36 +0300

    delete, add,refresh

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 
 const userListData =document.getElementById("users")
+const addUser=document.getElementsByClassName("add-user")
 const url ="https://63340612433198e79dcddf72.mockapi.io/api/users"
+
 
 //Method Get all data
 //---------------------------------------------------------------
@@ -25,13 +27,19 @@ let dataObject =(obj)=>
     <td>${i.surname}</td>
     <td>${i.city}</td>
     <td>
-    <input class="update-button" id="update-button" type="button" value="Update" >
-    <input class="update-button" id="delete-button" type="button" value="Delete" >
-</td>
+    
+    <form method="POST">
+    <a  onclick=deleteClick(${i.id})     class="delete-button" id="delete-button">Delete</a>
+    <a  onclick=updateUserClick(${i.id}) class="update-button" id="update-button">Update</a>
+
+    </form>
+    </td>
     </tr>`  
     userListData.innerHTML+=row
+ 
    
 }}
+
 //---------------------------------------------------------------
 
 const getAllData= async()=>
@@ -43,52 +51,108 @@ getAllData()
 
 //---------------------------------------------------------------
 // Method Get user get 
-const getUserId=(Id)=> {
-    return fetch(url+"/"+Id);
-}
+// const getUserFetch=(Id)=> {
+//     return fetch(url+"/"+Id);
+// }
 
-async function getUserDataId(userId) {
-    try {
-        const repo = await getUserId(userId);
-        return repo.json();
-    }
-    catch (e) {
-        console.log("Error found", e.message);
-        throw e;
-    }
-}
-console.log(getUserDataId(2).then(x =>{console.log(x.name)}))
+// const getUserDataId=async(userId)=> {
+//     try {
+//         const repo = await getUserFetch(userId);
+//         return repo.json();
+//     }
+//     catch (e) {
+//         console.log("Error found", e.message);
+//         throw e;
+//     }
+// }
+// console.log(getUserDataId(2).then(x =>{console.log(x.name)}))
 //--------------------------------------------------------------
 //Post request  
-//  function postUser()
-// {
-// return fetch(url, {
-//     method:'POST',
+  function postUser()
+ {
+ return fetch(url, {
+     method:'POST',
    
 
-// }).then(response =>response.json())
 
-// }
-// postUser().then(data =>console.log(data))
-// .catch(error=>console.error(error))
+ }).then(response =>response.json())
+
+ }
+
 //--------------------------------------------------------------
-
 
 //Delete User
 
-// function postDeletUserId(Id)
-// {
-//     return fetch(url+`/`+Id,{
-//         method:"DELETE",
+const userDeleteFetch=(Id)=>
+{
+    const requestOptions={
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify({ title: 'Fetch Detele Request Example' })
+    }
+    return fetch(url + `/` + Id,requestOptions)
+}
+ const userDelete=async(Id)=>
+ {
+    
+     const response = await userDeleteFetch(Id)
+     return await response.json()
+ }
 
-//     }).then(response =>response.json())
-// }
-
-// postDeletUserId(5).then(data=>console.log(data))
-// .catch(error=>console.log(error))
+//  userDelete(2).then(data=>console.log(data))
+//  .catch(error=>console.log(error))
 //--------------------------------------------------------------
+//delete click functiony
+const deleteClick=(Id)=>
+{
+    userDelete(Id);
+   
+}
+//--------------------------------------------------------------
+//update funciton 
 
+const updateUser= async(Id)=>
+{
+    const requestOptions = {
+        method:'PUT',
+        headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify({ title: 'Fetch PUT Request Example' })
+    }
+    try
+    {
+        const response = await fetch(url + `/` + Id, requestOptions);
+        return await response.json()
+    }
+    catch(e)
+    {
 
+        throw new Error("Error updateUser",e)
+    }
+    
+}
+
+const updateUserClick=(Id)=>
+{
+    updateUser(Id)
+}
+
+const userAddClick=()=>
+{
+    postUser().then(data =>console.log(data))
+    .catch(error=>console.error(error))
+}
+
+const refreshClick=()=>
+{
+    refresh()
+
+}
+
+const refresh=()=>
+{
+
+    location.reload();
+}
 
 
 
